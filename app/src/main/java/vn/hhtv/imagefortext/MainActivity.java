7275972be64a2cb06c0b5e85b8a0412d26abac91
VinhDN <vinhdn.hhtv@gmail.com>
2016-01-30 12:15:23 +0700

    update animation

--- app/src/main/java/vn/hhtv/imagefortext/MainActivity.java
+++ app/src/main/java/vn/hhtv/imagefortext/MainActivity.java
@@ -16,12 +16,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.ActionMode;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import java.io.File;
@@ -30,7 +39,7 @@ import java.io.FileOutputStream;
 import vn.hhtv.imagefortext.fragments.ImageFragment;
 import vn.hhtv.imagefortext.widget.AutoResizeEditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private AutoResizeEditText mContentEdt;
     private ViewPager mViewPager;
@@ -38,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
     private RelativeLayout mRootView;
     private String mContent = "";
     private Bitmap bitmap;
+    private static int timeMove = 200;
+    private static float distanceMove = 200f;
     private int timeDelay = 1 * 1000; // 1second
     private Runnable runCheck = new Runnable() {
         @Override
@@ -47,6 +58,8 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private ImageView mGridBtn, mFbBtn, mInsBtn, mTwBtn, mSettingBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,6 +68,16 @@ public class MainActivity extends AppCompatActivity {
         mViewPager = (ViewPager) findViewById(R.id.viewPager);
         mContentEdt = (AutoResizeEditText) findViewById(R.id.editText);
         mRootView = (RelativeLayout) findViewById(R.id.rootView);
+        mGridBtn = (ImageView) findViewById(R.id.grid);
+        mGridBtn.setOnClickListener(this);
+        mFbBtn = (ImageView) findViewById(R.id.fb);
+        mFbBtn.setOnClickListener(this);
+        mInsBtn = (ImageView) findViewById(R.id.inta);
+        mInsBtn.setOnClickListener(this);
+        mTwBtn = (ImageView) findViewById(R.id.tw);
+        mTwBtn.setOnClickListener(this);
+        mSettingBtn = (ImageView) findViewById(R.id.setting);
+        mSettingBtn.setOnClickListener(this);
         mRootView.setDrawingCacheEnabled(false);
         mContentEdt.addTextChangedListener(contentChange);
         mContentEdt.setOnTouchListener(new View.OnTouchListener() {
@@ -67,24 +90,24 @@ public class MainActivity extends AppCompatActivity {
         mContentEdt.setHeightLimit(500);
         mViewPager.setOffscreenPageLimit(4);
         mContentEdt.setSelected(false);
-        if(Build.VERSION.SDK_INT > 10)
-        mContentEdt.setCustomSelectionActionModeCallback(new ActionMode.Callback() {
+        if (Build.VERSION.SDK_INT > 10)
+            mContentEdt.setCustomSelectionActionModeCallback(new ActionMode.Callback() {
 
-            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
-                return false;
-            }
+                public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                    return false;
+                }
 
-            public void onDestroyActionMode(ActionMode mode) {
-            }
+                public void onDestroyActionMode(ActionMode mode) {
+                }
 
-            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-                return false;
-            }
+                public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                    return false;
+                }
 
-            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-                return false;
-            }
-        });
+                public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                    return false;
+                }
+            });
         mViewPager.setAdapter(new ImagePageAdapter(getSupportFragmentManager()));
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -119,9 +142,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void checkContent(){
+    private void checkContent() {
         String text = mContentEdt.getText().toString();
-        if(mContent.equals(text)){
+        if (mContent.equals(text)) {
             return;
         }
         mContent = text;
@@ -136,8 +159,8 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
             mHandler.removeCallbacks(runCheck);
-            if(count > 0)
-            mHandler.postDelayed(runCheck, timeDelay);
+            if (count > 0)
+                mHandler.postDelayed(runCheck, timeDelay);
         }
 
         @Override
@@ -146,7 +169,118 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    private class ImagePageAdapter extends FragmentPagerAdapter{
+    private boolean isExpand = false;
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.grid:
+                mFbBtn.startAnimation(moveLeftToRight(isExpand));
+                mSettingBtn.startAnimation(moveBottomToTop(isExpand));
+                mInsBtn.startAnimation(moveToPosition(6f, isExpand));
+                mTwBtn.startAnimation(moveToPosition(3f, isExpand));
+                isExpand = !isExpand;
+                break;
+            case R.id.fb:
+                v.startAnimation(scaleClick());
+                break;
+            case R.id.tw:
+                v.startAnimation(scaleClick());
+                break;
+            case R.id.inta:
+                v.startAnimation(scaleClick());
+                break;
+            case R.id.setting:
+                v.startAnimation(scaleClick());
+                break;
+
+        }
+    }
+
+    private static Animation moveLeftToRight(boolean reverse) {
+        AnimationSet animationSet = new AnimationSet(true);
+        Animation moveLefttoRight = new TranslateAnimation(reverse ? -distanceMove : 0,reverse ? 0 : -distanceMove, 0, 0);
+        moveLefttoRight.setDuration(timeMove);
+        moveLefttoRight.setFillAfter(true);
+        Animation aSc = new ScaleAnimation(0.0f, 1.0f, 0.0f, 1.0f,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        if(reverse){
+            animationSet.addAnimation(new ScaleAnimation(1.0f, 0.0f, 1.0f, 0.0f,
+                    Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f));
+        }
+        aSc.setDuration(timeMove);
+        aSc.setFillAfter(true);
+        animationSet.addAnimation(aSc);
+        animationSet.addAnimation(moveLefttoRight);
+        Animation alp = new AlphaAnimation(reverse? 1.0f : 0.0f, reverse ? 0.0f : 1.0f);
+        alp.setDuration(timeMove);
+        alp.setFillAfter(true);
+        animationSet.addAnimation(alp);
+        animationSet.setFillAfter(true);
+        return animationSet;
+    }
+
+    private static Animation scaleClick() {
+        AnimationSet animationSet = new AnimationSet(true);
+        Animation aSc = new ScaleAnimation(1.0f, 2.0f, 1.0f, 2.0f,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        aSc.setDuration(timeMove);
+        aSc.setFillAfter(true);
+        animationSet.addAnimation(aSc);
+        animationSet.setFillAfter(true);
+        return animationSet;
+    }
+
+    private static Animation moveBottomToTop(boolean reverse) {
+        AnimationSet animationSet = new AnimationSet(true);
+        Animation moveLefttoRight = new TranslateAnimation(0, 0, reverse ? -distanceMove : 0,reverse ? 0 : -distanceMove);
+        moveLefttoRight.setDuration(timeMove);
+        moveLefttoRight.setFillAfter(false);
+        Animation aSc = new ScaleAnimation(0.0f, 1.0f, 0.0f, 1.0f,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        if(reverse){
+            animationSet.addAnimation(new ScaleAnimation(1.0f, 0.0f, 1.0f, 0.0f,
+                    Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f));
+        }
+        aSc.setDuration(timeMove);
+        aSc.setFillAfter(true);
+        animationSet.addAnimation(aSc);
+        animationSet.addAnimation(moveLefttoRight);
+        Animation alp = new AlphaAnimation(reverse? 1.0f : 0.0f, reverse ? 0.0f : 1.0f);
+        alp.setDuration(timeMove);
+        alp.setFillAfter(true);
+        animationSet.addAnimation(alp);
+        animationSet.setFillAfter(false);
+        return animationSet;
+    }
+
+    private static Animation moveToPosition(float p, boolean reverse) {
+        AnimationSet animationSet = new AnimationSet(true);
+        Animation moveLefttoRight = new TranslateAnimation(0f, -(float) (distanceMove * Math.cos(Math.PI / p)), 0f, -(float) (distanceMove * Math.sin(Math.PI / p)));
+        if(reverse){
+            moveLefttoRight = new TranslateAnimation(-(float) (distanceMove * Math.cos(Math.PI / p)), 0f, -(float) (distanceMove * Math.sin(Math.PI / p)),0f);
+        }
+        moveLefttoRight.setDuration(timeMove);
+        moveLefttoRight.setFillAfter(true);
+        Animation aSc = new ScaleAnimation(0.0f, 1.0f, 0.0f, 1.0f,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        if(reverse){
+            animationSet.addAnimation(new ScaleAnimation(1.0f, 0.0f, 1.0f, 0.0f,
+                    Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f));
+        }
+        aSc.setDuration(timeMove);
+        aSc.setFillAfter(true);
+        animationSet.addAnimation(moveLefttoRight);
+        animationSet.addAnimation(aSc);
+        Animation alp = new AlphaAnimation(reverse? 1.0f : 0.0f, reverse ? 0.0f : 1.0f);
+        alp.setDuration(timeMove);
+        alp.setFillAfter(true);
+        animationSet.addAnimation(alp);
+        animationSet.setFillAfter(true);
+        return animationSet;
+    }
+
+    private class ImagePageAdapter extends FragmentPagerAdapter {
 
         public ImagePageAdapter(FragmentManager fm) {
             super(fm);
